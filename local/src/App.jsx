d90404fbfba0e815414eddad539433e142356ce0
ashishparihar679 <ashishparihar679@gmail.com.>
2025-11-28 18:39:03 +0530

    first commit

---
+++ local/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import Form from './Form'
+
+function App() {
+
+  return (
+    <>
+    <h1>hello</h1>
+    <Form/>
+    
+    </>
+  )
+}
+
+export default App
---
+++ local/src/Form.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from 'react'
+
+const Form = () => {
+
+    let [frmdata,setfrmdata]= useState({})
+    // let [data,setdata]= useState([])
+
+    function datasubmit(e){
+        const {name,value} = e.target
+        setfrmdata({...frmdata,[name]:value})
+    }
+    function handlsubmit(e){
+        e.preventDefault()
+        console.log(frmdata)
+        localStorage.setItem('userdata',JSON.stringify(frmdata))
+    }
+
+    // const get = () => {
+        let a = JSON.parse(localStorage.getItem('userdata'))
+        // const a=JSON.parse(localStorage.getItem("userdata"));
+        console.log(a)
+        console.log(a.username)
+        // setdata(a)
+    // }
+    // get()
+  return (
+    <>
+    
+    {/* <div>Form</div> */}
+    <form action="" onSubmit={handlsubmit}>
+
+        <label htmlFor="">user name</label>
+        <input type="text" name='username' onChange={datasubmit} /> <br /> <br />
+
+         <label htmlFor="">user email</label>
+        <input type="text" name='useremail' onChange={datasubmit} /> <br /> <br />
+
+         <label htmlFor="">user pass</label>
+        <input type="text" name='userpass' onChange={datasubmit} /> <br /> <br />
+
+        <input type="submit" />
+    </form>
+    </>
+  )
+}
+
+export default Form
